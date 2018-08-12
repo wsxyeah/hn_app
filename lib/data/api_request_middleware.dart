14a43106362e8c455c7e71e8d921f894e0c0d0c1
Shaoxing Wang <sxwang.me@gmail.com>
2018-08-12 09:03:21 +0800

    Add naive memory cache

--- lib/data/api_request_middleware.dart
+++ lib/data/api_request_middleware.dart
@@ -4,6 +4,8 @@ import 'package:hn_app/data/actions.dart';
 import 'package:hn_app/data/entities.dart';
 import 'package:redux/redux.dart';
 
+var _storyCache = new Map<int, Story>();
+
 class ApiRequestMiddleWare<S> implements MiddlewareClass<S> {
   @override
   void call(Store<S> store, action, NextDispatcher next) async {
@@ -11,9 +13,15 @@ class ApiRequestMiddleWare<S> implements MiddlewareClass<S> {
       if (action.forRequest) {
         next(action);
         Iterable<int> ids = (await HnApi().topStoryIds()).take(10);
-        List<Story> items = await Future.wait(ids.map((id) => HnApi().item(id)));
+        List<Story> items = await Future.wait(ids.map((id) =>
+            _storyCache.containsKey(id)
+                ? Future.value(_storyCache[id])
+                : HnApi().item(id)));
         print("[items]: $items");
         store.dispatch(FetchStoriesAction(false, items, null));
+
+        items.forEach((s) => _storyCache[s.id] = s);
+        print("[cache]: $_storyCache");
         return;
       }
     }
