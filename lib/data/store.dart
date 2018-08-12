df193a16ed078808e8062e7d7aa0144e51d2d279
Shaoxing Wang <sxwang.me@gmail.com>
2018-08-12 19:15:23 +0800

    Add redux logging

--- lib/data/store.dart
+++ lib/data/store.dart
@@ -3,8 +3,10 @@ import 'package:hn_app/data/entities.dart';
 import 'package:hn_app/data/reducers.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_dev_tools/redux_dev_tools.dart';
+import 'package:redux_logging/redux_logging.dart';
 
 Store<AppState> configureStore() {
   return new DevToolsStore<AppState>(reducer,
-      initialState: AppState.initial(), middleware: [ApiRequestMiddleWare()]);
+      initialState: AppState.initial(),
+      middleware: [ApiRequestMiddleWare(), LoggingMiddleware.printer()]);
 }
